b36a5a7bed6674947bbc97bbf7471cc320e1efd1
Akhileshwar Pandey <akhileshwar242000@gmail.com>
2025-06-17 21:30:55 +0530

    test commit

--- backend/app/api/example.py
+++ backend/app/api/example.py
@@ -4,4 +4,7 @@ from flask_restful import Resource
 class Index(Resource):
     def get(self):
         return jsonify({"msg":"Hello World!"})
+class Inde(Resource):
+    def get(self):
+        return jsonify({"msg":"Hello asdf!"})
         
