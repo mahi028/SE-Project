fdbc11784ba6a75df431cd37ff33c12762cfdc00
mahi028 <tewarimohit028@gmail.com>
2025-06-21 05:28:27 +0530

    Removed unessesory components, refactored router and layout.

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -4,6 +4,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
+import VitePluginVueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        VitePluginVueDevTools(),
         Components({
             resolvers: [PrimeVueResolver()]
         })
