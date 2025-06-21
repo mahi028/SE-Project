7166ead7201966856a38fe7390e2735cd7bd358c
mahi028 <tewarimohit028@gmail.com>
2025-06-21 22:55:55 +0530

    Dashboard Directory

--- frontend/src/views/pages/dashboards/SeniorDashboard.vue
+++ frontend/src/views/pages/dashboards/SeniorDashboard.vue
@@ -3,12 +3,12 @@ import BestSellingWidget from '@/components/dashboard/BestSellingWidget.vue';
 import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue';
 import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
 import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
-import StatsWidget from '@/components/dashboard/StatsWidget.vue';
+import QuickButtons from '@/components/QuickButtons.vue';
 </script>
 
 <template>
     <div class="grid grid-cols-12 gap-8">
-        <StatsWidget />
+        <QuickButtons page="senior"/>
 
         <div class="col-span-12 xl:col-span-6">
             <RecentSalesWidget />
