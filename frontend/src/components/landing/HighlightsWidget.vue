a94dddf3e850e4838000a53ee447a10efea4b073
rashisinghal16 <21F1005286@ds.study.iitm.ac.in>
2025-06-22 01:05:50 +0530

    text done.

--- frontend/src/components/landing/HighlightsWidget.vue
+++ frontend/src/components/landing/HighlightsWidget.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="highlights" class="py-6 px-6 lg:px-20 mx-0 my-12 lg:mx-20">
         <div class="text-center">
-            <div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-4xl">Powerful Everywhere</div>
-            <span class="text-muted-color text-2xl">Amet consectetur adipiscing elit...</span>
+            <div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-4xl">Support at Your Fingertips</div>
+            <span class="text-muted-color text-2xl">Designed for ease, security, and connection — wherever you are....</span>
         </div>
 
         <div class="grid grid-cols-12 gap-4 mt-20 pb-2 md:pb-20">
@@ -14,10 +14,9 @@
                 <div class="flex items-center justify-center bg-purple-200 self-center lg:self-end" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
                     <i class="pi pi-fw pi-mobile !text-4xl text-purple-700"></i>
                 </div>
-                <div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">Congue Quisque Egestas</div>
+                <div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">Accessible Anywhere</div>
                 <span class="text-surface-700 dark:text-surface-100 text-2xl leading-normal ml-0 md:ml-2" style="max-width: 650px"
-                    >Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Sit amet mattis vulputate enim nulla aliquet.</span
-                >
+                    >Senior users can view reminders, emergency contacts, and appointments right from their phones — anytime, anywhere....</span>
             </div>
         </div>
 
@@ -26,10 +25,9 @@
                 <div class="flex items-center justify-center bg-yellow-200 self-center lg:self-start" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
                     <i class="pi pi-fw pi-desktop !text-3xl text-yellow-700"></i>
                 </div>
-                <div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">Celerisque Eu Ultrices</div>
+                <div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">Caregiver Dashboard</div>
                 <span class="text-surface-700 dark:text-surface-100 text-2xl leading-normal mr-0 md:mr-2" style="max-width: 650px"
-                    >Adipiscing commodo elit at imperdiet dui. Viverra nibh cras pulvinar mattis nunc sed blandit libero. Suspendisse in est ante in. Mauris pharetra et ultrices neque ornare aenean euismod elementum nisi.</span
-                >
+                    >Family members and professionals can monitor logs, updates, and schedule care seamlessly using the web dashboard.</span                >
             </div>
 
             <div class="flex justify-end order-1 sm:order-2 col-span-12 lg:col-span-6 bg-yellow-100 p-0" style="border-radius: 8px">
