33b056119f3d0a63c129e94d12050b0bd89bc197
mahi028 <tewarimohit028@gmail.com>
2025-06-21 23:29:17 +0530

    Fixed Quick button behaviour

--- frontend/src/views/pages/dashboards/DoctorDashbaord.vue
+++ frontend/src/views/pages/dashboards/DoctorDashbaord.vue
@@ -4,12 +4,18 @@ import DoctorNotificationsWidget from '@/components/doctorDashboard/DoctorNotifi
 import DoctorAppointments from '@/components/doctorDashboard/DoctorAppointments.vue';
 import DoctorSchedule from '@/components/doctorDashboard/DoctorSchedule.vue';
 import QuickButtons from '@/components/QuickButtons.vue';
+import Divider from 'primevue/divider';
 </script>
 
 <template>
     <div class="grid grid-cols-12 gap-8">
         <QuickButtons page="doctor"/>
 
+        <div class="col-span-12 xl:col-span-12">
+            <Divider align="left" type="solid">
+                <h3>Quick Widgets</h3>
+            </Divider>
+        </div>
         <div class="col-span-12 xl:col-span-6">
             <DoctorAppointments />
             <AppointmentRequests />
