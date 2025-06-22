2f77bd03e676e78c139427f1d98c30de3aaa1de3
mahi028 <tewarimohit028@gmail.com>
2025-06-23 00:55:58 +0530

    Added Pinia state management store.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
@@ -9,8 +10,10 @@ import ToastService from 'primevue/toastservice';
 
 import '@/assets/styles.scss';
 
+const pinia = createPinia()
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 app.use(PrimeVue, {
     theme: {
