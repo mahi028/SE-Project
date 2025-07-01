da990d42f4ac22371914aab2f8070853f37ac375
22f3002778 <22f3002778@ds.study.iitm.ac.in>
2025-07-01 14:38:35 +0530

    Commited issue Update Senior Dashboard #53

--- frontend/src/service/NotificationService.js
+++ frontend/src/service/NotificationService.js
@@ -5,10 +5,17 @@ export const notificationService = {
                     {type: 'Appointment', name: 'Mr. Mishra', time: '5:00 PM', time_left:'1 Hour'},
                     {type: 'Appointment', name: 'Mr. Chopra', time: '5:30 PM', time_left:'1 Hour 30 Min'},
                     {type: 'Vital', name: 'Mr. Chopra', label: 'Blood Pressure', serverity: 'low'},
+
+                    { type: 'Reminder', label: 'Drink Water', time: '4:00 PM' },
+                    { type: 'Reminder', label: 'Walk Time', time: '6:00 AM' },
+                    { type: 'Reminder', label: 'Take Medicine', time: '9:00 AM' }
                 ],
                 yesterday: [
                     {type: 'Appointment', name: 'Mr. Mishra', time: '5:00 PM', time_left:'1 day'},
                     {type: 'Appointment', name: 'Mr. Chopra', time: '5:30 PM', time_left:'1 day'},
+
+                    { type: 'Reminder', label: 'Drink Water', time: '4:00 PM' },
+                    { type: 'Reminder', label: 'Evening Walk', time: '6:30 PM' }
                 ]
             }
     },
