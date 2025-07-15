5558b175c2b28147a1d02d16f8c0c77c510b416f
mahi028 <tewarimohit028@gmail.com>
2025-07-15 06:25:14 +0530

    JWT Setup and Login/Register Query/Mutations Setup

--- backend/app/graphql/__init__.py
+++ backend/app/graphql/__init__.py
@@ -1,11 +1,13 @@
 import graphene
 from .users import GetUsers, AddUser
+from .auth import GetToken, Register
 
-class Query(GetUsers, graphene.ObjectType):
+class Query(GetUsers, GetToken, graphene.ObjectType):
     pass
 
 class Mutation(graphene.ObjectType):
     add_user = AddUser.Field()
+    register_user = Register.Field()
 
 schema = graphene.Schema(
                             query=Query,
