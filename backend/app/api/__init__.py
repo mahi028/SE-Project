f4b762a602cef452c709ac6b4b659613d40953bf
mahi028 <tewarimohit028@gmail.com>
2025-07-17 07:47:18 +0530

    unnecessary files

--- backend/app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Api
-
-api = Api(prefix='/api') # change it to api
-
-# add Api resouces here. Example:
-
-from .example import Index
-api.add_resource(Index, '/example')
--- backend/app/api/example.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from flask_restful import Resource
-
-class Index(Resource):
-    def get(self):
-        return jsonify({"msg":"Hello World!"})
-
-        
